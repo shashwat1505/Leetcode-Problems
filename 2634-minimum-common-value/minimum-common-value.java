f0793920dbcc92961930132f3ded59ee739113cb
Shashwat Shahi <120392889+shashwat1505@users.noreply.github.com>
2025-02-12 23:10:29 +0530

    Time: 2 ms (86.37%) | Memory: 62.7 MB (56.67%) - LeetSync

---
+++ 2634-minimum-common-value/minimum-common-value.java
@@ -0,0 +1,34 @@
+class Solution {
+
+    /**
+     * Finds the first common element in two sorted arrays.
+     *
+     * If a common element is found, this method returns that element.
+     * If there are no common elements, the method returns -1.
+     *
+     * @param nums1 The first sorted array.
+     * @param nums2 The second sorted array.
+     * @return The first common element or -1 if none found.
+     */
+    public int getCommon(int[] nums1, int[] nums2) {
+        int nums1Length = nums1.length;
+        int nums2Length = nums2.length;
+
+        // Initialize indices for iterating through the arrays
+        int index1 = 0;
+        int index2 = 0;
+
+        // Loop through both arrays until one array is fully traversed
+        while (index1 < nums1Length && index2 < nums2Length) {
+            if (nums1[index1] == nums2[index2]) {
+                return nums1[index1];
+            }
+            if (nums1[index1] < nums2[index2]) {
+                ++index1;
+            } else {
+                ++index2;
+            }
+        }
+        return -1;
+    }
+}
