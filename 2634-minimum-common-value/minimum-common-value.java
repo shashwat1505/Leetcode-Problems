794b38a9def8d595ddf02cb76dc36f09a0710174
Shashwat Shahi <120392889+shashwat1505@users.noreply.github.com>
2025-02-12 23:13:11 +0530

    Time: 1 ms (96.95%) | Memory: 62.5 MB (78.46%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.java
+++ 2634-minimum-common-value/minimum-common-value.java
@@ -6,15 +6,13 @@ class Solution {
      * If a common element is found, this method returns that element.
      * If there are no common elements, the method returns -1.
      *
-     * @param nums1 The first sorted array.
-     * @param nums2 The second sorted array.
-     * @return The first common element or -1 if none found.
+     * @param nums1
+     * @param nums2
+     * @return
      */
     public int getCommon(int[] nums1, int[] nums2) {
         int nums1Length = nums1.length;
         int nums2Length = nums2.length;
-
-        // Initialize indices for iterating through the arrays
         int index1 = 0;
         int index2 = 0;
         while (index1 < nums1Length && index2 < nums2Length) {
