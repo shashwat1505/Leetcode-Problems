3cc4f6d7f308b540d43bfcd0be608dbd14029934
Shashwat Shahi <120392889+shashwat1505@users.noreply.github.com>
2025-03-15 21:02:05 +0530

    Time: 1 ms (100.00%) | Memory: 41.5 MB (18.85%) - LeetSync

--- 728-self-dividing-numbers/self-dividing-numbers.java
+++ 728-self-dividing-numbers/self-dividing-numbers.java
@@ -3,13 +3,13 @@ class Solution {
     List<Integer> ans = new ArrayList<>();
 
     for (int num = left; num <= right; ++num)
-      if (dividingNumber(num))
+      if (dividingnumber(num))
         ans.add(num);
 
     return ans;
   }
 
-  private boolean dividingNumber(int num) {
+  private boolean dividingnumber(int num) {
     for (int n = num; n > 0; n /= 10)
       if (n % 10 == 0 || num % (n % 10) != 0)
         return false;
