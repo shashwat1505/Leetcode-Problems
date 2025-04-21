000c9200865fcd0b08c753cf52ca58014097796d
Shashwat Shahi <120392889+shashwat1505@users.noreply.github.com>
2025-04-21 07:33:03 +0530

    Time: 2 ms (99.27%) | Memory: 48.2 MB (42.10%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -7,36 +7,32 @@ class Solution {
         int cnt1 = 0, cnt2 = 0;
         int el1 = Integer.MIN_VALUE;
         int el2 = Integer.MIN_VALUE;
-
-        // First pass: find potential candidates
         for (int i = 0; i < n; i++) {
             if (nums[i] == el1) {
-                cnt1++;
-            } else if (nums[i] == el2) {
-                cnt2++;
-            } else if (cnt1 == 0) {
-                el1 = nums[i];
-                cnt1 = 1;
-            } else if (cnt2 == 0) {
-                el2 = nums[i];
-                cnt2 = 1;
-            } else {
-                cnt1--;
-                cnt2--;
-            }
+            cnt1++;
+        } else if (nums[i] == el2) {
+        cnt2++;
         }
-
-        // Second pass: verify the candidates
-        cnt1 = 0;
-        cnt2 = 0;
-        for (int num : nums) {
-            if (num == el1) cnt1++;
-            if (num == el2) cnt2++;
+        else if (cnt1 == 0) {
+            el1 = nums[i];
+            cnt1 = 1;
+        } else if (cnt2 == 0) {
+            el2 = nums[i];
+            cnt2 = 1;
         }
-
-        List<Integer> result = new ArrayList<>();
-        if (cnt1 > n / 3) result.add(el1);
-        if (cnt2 > n / 3) result.add(el2);
-        return result;
+        else {
+            cnt1--; cnt2--;
+        }
     }
-}
+    List<Integer>ls = new ArrayList<>();
+    cnt1 = 0; cnt2 = 0;
+    for (int i =0; i < n; i++) {
+        if (nums[i] == el1) cnt1++;
+        if (nums[i] == el2) cnt2++;
+    }
+    int mini = (int)(n / 3) + 1;
+    if (cnt1 >= mini) ls.add(el1);
+    if (cnt2 >= mini) ls.add(el2);
+    return ls;
+  }
+}  
