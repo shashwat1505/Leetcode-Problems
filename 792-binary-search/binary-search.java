70965927c139cf55ccea7ae72402b792a1e9a304
Shashwat Shahi <120392889+shashwat1505@users.noreply.github.com>
2025-07-05 01:27:37 +0530

    Time: 0 ms (100.00%) | Memory: 46.3 MB (7.49%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,6 +1,14 @@
-class Solution {
-  public int search(int[] nums, int target) {
-    final int i = Arrays.binarySearch(nums, target);
-    return i < 0 ? -1 : i;
+import java.util.*;
+public class Solution {
+  public static int binarySearch(int[] nums, int low, int high, int target) {
+    if (low > high) return -1;
+    int mid = (low + high) / 2;
+    if (nums[mid] == target) return mid;
+    else if (target > nums[mid])
+    return binarySearch (nums, mid + 1, high, target);
+    return binarySearch (nums, low, mid - 1, target);
   }
-}
+  public static int search (int [] nums, int target) {
+    return binarySearch (nums, 0, nums.length - 1, target);
+  }
+  }
