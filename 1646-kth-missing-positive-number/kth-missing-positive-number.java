7260bf658e2d2a95c91cd76cc28ceb43d4900a18
Shashwat Shahi <120392889+shashwat1505@users.noreply.github.com>
2025-07-29 02:42:46 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (45.93%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.java
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.java
@@ -1,9 +1,9 @@
 import java.util.*;
 public class Solution {
     public int findKthPositive(int[] arr, int k) {
-        int n = arr.length; 
+        int n = arr.length; // size/length of array:
         int low = 0; 
-        int high = n - 1;
+        int high = n - 1; //initialize high as n - 1:
         while (low <= high) {
             int mid = (low + high) / 2;
             int missing = arr[mid] - (mid + 1);
